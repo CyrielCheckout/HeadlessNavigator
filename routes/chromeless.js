c7d9ab0dfdce0acc6b92c1a7f59e8ff77b5e4cd6
CyrielCheckout <cyrielghendrih-feillant@checkout.com>
2022-05-19 11:53:33 +0200

    Add girpoay

--- routes/chromeless.js
+++ routes/chromeless.js
@@ -266,4 +266,56 @@ router.post('/P24', async function(req, res, next) {
             .json(err);
     }
 });
+
+router.post('/GiroPay', async function(req, res, next) {
+    console.log('Got url:', req.url);
+    console.log("Request Params: ", req.body);
+    try {
+        browser = await puppeteer.launch({ headless: req.body.Headless, executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome", defaultViewport: null });
+        const page = await browser.newPage();
+        const goto = await page.goto(req.body.RedirectURL, { waitUntil: 'networkidle0' });
+        await page.type('#tags', 'TESTDETT421', { delay: 100 });
+        await page.waitForSelector('#ui-id-2');
+        const button1 = await page.$('#ui-id-2');
+        await button1.evaluate(b => b.click());
+        await page.click("#idtoGiropayDiv > input", { button: "left" });
+        await page.click("#yes", { button: "left" });
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await page.type('#XjAAADYfePJarVlf', 'chiptanscatest2', { delay: 100 });
+        await page.type('#WdGSSNPyXoBJOdNW', '12345', { delay: 100 });
+        await page.click("body > section > div > div > div.contentcontainerMainLayout > div.loginformgrund > div > form > div > div.stdBoxLayout.osppinfoinhalt.floatLeft > div > input", { button: "left" });
+        const lastPosition = await scrollPageToBottom(page, {
+            size: 500,
+            delay: 250
+        });
+        const button2 = await page.$('input[name="weiterButton"]');
+        await button2.evaluate(b => b.click());
+        await page.waitForSelector('#iwmEuVJUMIPRnoma');
+        await page.type('#iwmEuVJUMIPRnoma', '123456', { delay: 100 });
+        const button3 = await page.$('body > section > div > div > div > div.stdBoxLayout > div.osppbuttonbereich > div.buttonWrap.hiddenXsSpk > input');
+        await button3.evaluate(b => b.click());
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        const lastPosition2 = await scrollPageToBottom(page, {
+            size: 500,
+            delay: 250
+        });
+        const button4 = await page.$('input[name="weiterButton"]');
+        await button4.evaluate(b => b.click());
+        await page.waitForSelector('#iwmEuVJUMIPRnoma');
+        await page.type('#iwmEuVJUMIPRnoma', '123456', { delay: 100 });
+        const button5 = await page.$('body > section > div > div > div > div.stdBoxLayout > div.osppbuttonbereich > div.buttonWrap.hiddenXsSpk > input');
+        await button5.evaluate(b => b.click());
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await browser.close();
+        res
+            .status(200)
+            .json({ "Status": "success" });
+    } catch (err) {
+        console.log(err);
+        res
+            .status(500)
+            .json(err);
+    }
+});
 module.exports = router;
